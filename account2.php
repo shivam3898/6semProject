8a8a4608070672d6d67676e257bba16a68b016a9
admin <37464913+shivam3898@users.noreply.github.com>
2019-04-28 09:51:00 +0530

    account page changes

--- account2.php
+++ account2.php
@@ -23,6 +23,7 @@ $retval = $mysqli->query($sql);
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="style.css" />
+	<script src="back.js"></script>
 	<title>ASS Gaming</title>
 </head>
 
